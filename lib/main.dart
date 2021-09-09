d26fbd02eb0b2b6238924819cbe8f463fa78b086
AllenAlexander98 <73653888+AllenAlexander98@users.noreply.github.com>
2021-09-10 06:14:02 +0700

    My First Dice Rolling App

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.deepOrangeAccent,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('Dice Roll'),
+          backgroundColor: Colors.deepOrangeAccent,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,62 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceRoll = 1;
+  int rightDiceRoll = 1;
+  int score = 0;
+
+  void rollingDice() {
+    leftDiceRoll = Random().nextInt(6) + 1;
+    rightDiceRoll = Random().nextInt(6) + 1;
+    score = leftDiceRoll + rightDiceRoll;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        SizedBox(
+          height: 100,
+        ),
+        Text(
+          '$score',
+          style: TextStyle(fontSize: 100, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      rollingDice();
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceRoll.png')),
+            ),
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      rollingDice();
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceRoll.png')),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 100,
+        )
+      ],
+    );
   }
 }
